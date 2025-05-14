33c3c14fe4ef58bbe7aee995e006d33fcd64a8ba
GLENDER2004 <glender.samuel06@gmail.com>
2025-05-14 07:20:53 -0500

    pruebas

---
+++ src/main/java/practica/practias/PractiasApplication.java
@@ -0,0 +1,13 @@
+package practica.practias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractiasApplication.class, args);
+	}
+
+}
